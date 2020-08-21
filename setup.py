9c39b6e68ca8efeeb3b6ca0333ef8af3dbb53ef6
Evan Bolyen <ebolyen@gmail.com>
2020-08-20 17:21:58 -0700

    DIST: fix package_data

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
                            'data/summarize_empty/empty_single_end/*',
                            'data/summarize_empty/empty_forward_in_paired/*',
                            'data/summarize_empty/empty_reverse_in_paired/*',
-                           'data/summarize_empty/empty_paired_end/*'
+                           'data/summarize_empty/empty_paired_end/*',
                            'data/reverse_only/*'
                            ],
         'q2_demux': ['_summarize/assets/*.html',
